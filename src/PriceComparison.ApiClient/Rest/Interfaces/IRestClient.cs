073af857a88b184fc7e7e3a89b9a74d96d86ced1
MrBelix <vlad.bilevskyi@gmail.com>
2024-06-30 00:40:59 +0300

    added access token to rest interface

--- src/PriceComparison.ApiClient/Rest/Interfaces/IRestClient.cs
+++ src/PriceComparison.ApiClient/Rest/Interfaces/IRestClient.cs
@@ -1,6 +1,15 @@
+using PriceComparison.Contracts.Authentication;
+
 namespace PriceComparison.ApiClient.Rest.Interfaces;
 
 public interface IRestClient
 {
+
+    public AccessTokenResponse? AccessToken { get; }
+
+    public event EventHandler<AccessTokenResponse?> AccessTokenChanged;
+
     public IAuthModule Authentication { get; }
+
+    public void SetAccessToken(AccessTokenResponse? accessToken);
 }
