c6a3af90277b03b6e84d2ee29e645a3f708311a9
MrBelix <vlad.bilevskyi@gmail.com>
2024-06-30 17:23:15 +0300

    fix

--- src/PriceComparison.ApiClient/Common/ResponseError.cs
+++ src/PriceComparison.ApiClient/Common/ResponseError.cs
@@ -4,7 +4,7 @@ namespace PriceComparison.ApiClient.Common;
 
 public record ResponseError
 {
-    public string? Message { get; init; }
-    public string? Code { get; init; }
-    public IImmutableDictionary<string, IImmutableList<string>>? Errors { get; init; }
+    public required string Message { get; init; }
+    public required string Code { get; init; }
+    public IImmutableDictionary<string, IImmutableList<string>> Errors { get; init; } = ImmutableDictionary<string, IImmutableList<string>>.Empty;
 }
