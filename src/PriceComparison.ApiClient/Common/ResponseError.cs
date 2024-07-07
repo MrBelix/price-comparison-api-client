1ddda6f0893b39e47b2174f03316c38af29b5508
MrBelix <vlad.bilevskyi@gmail.com>
2024-07-07 03:24:57 +0300

    updated response error

--- src/PriceComparison.ApiClient/Common/ResponseError.cs
+++ src/PriceComparison.ApiClient/Common/ResponseError.cs
@@ -4,7 +4,7 @@ namespace PriceComparison.ApiClient.Common;
 
 public record ResponseError
 {
-    public required string Message { get; init; }
-    public required string Code { get; init; }
+    public required string Detail { get; init; }
+    public required string Status { get; init; }
     public IImmutableDictionary<string, IImmutableList<string>> Errors { get; init; } = ImmutableDictionary<string, IImmutableList<string>>.Empty;
 }
